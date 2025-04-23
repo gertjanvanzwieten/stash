0677ae7a23a6ed9d974aa380b6a819a1aa35cf33
Gertjan van Zwieten <gertjanvanzwieten@evalf.com>
2025-04-23 11:08:51 +0200

    Update unit tests

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import stash, math, unittest
+import stash, math, unittest, tempfile
 
 try:
     import numpy
@@ -18,25 +18,23 @@ class MyReduceableClass(MyClass):
         return MyReduceableClass, (self.x,)
 
 
-class Stash(unittest.TestCase):
+class Base(unittest.TestCase):
 
     def check(self, obj, eq=lambda x: x):
-        d = {}
-        db = stash.PyDB(d)
-        b = db.hash(obj)
-        obj_ = db.unhash(b)
+        h = self.db.hash(obj)
+        obj_ = self.db.unhash(h)
         self.assertIs(type(obj), type(obj_))
         self.assertEqual(eq(obj_), eq(obj))
-        return len(d[b])
+        return h
 
     def test_int(self):
-        self.assertEqual(self.check(-1), 2)
-        self.assertEqual(self.check(0), 1)
-        self.assertEqual(self.check(1), 2)
-        self.assertEqual(self.check(127), 2)
-        self.assertEqual(self.check(128), 3)
-        self.assertEqual(self.check(-128), 2)
-        self.assertEqual(self.check(-129), 3)
+        self.check(-1)
+        self.check(0)
+        self.check(1)
+        self.check(127)
+        self.check(128)
+        self.check(-128)
+        self.check(-129)
 
     def test_float(self):
         self.check(0.)
@@ -73,10 +71,10 @@ class Stash(unittest.TestCase):
 
     def test_dict(self):
         d1 = {'a': 1, 'b': 2, 'c': 3}
-        self.check(d1)
+        h1 = self.check(d1)
         d2 = {'b': 2, 'c': 3, 'a': 1}
-        self.check(d2)
-        self.assertEqual(stash.hash(d1), stash.hash(d2))
+        h2 = self.check(d2)
+        self.assertEqual(h1, h2)
 
     def test_none(self):
         self.check(None)
@@ -103,3 +101,65 @@ class Stash(unittest.TestCase):
     @unittest.skipIf(numpy is None, "numpy is not installed")
     def test_dispatch_table(self):
         self.check(numpy.sin)
+
+
+class Nil(Base):
+
+    def check(self, obj, eq=lambda x: x):
+        return stash.hash(obj)
+
+
+class PyDB(Base):
+
+    def setUp(self):
+        self.d = {}
+        self.db = stash.PyDB(self.d)
+
+    def assertLength(self, obj, n):
+        h = self.db.hash(obj)
+        self.assertEqual(len(self.d[h]), n)
+
+    def test_int(self):
+        self.assertLength(-1, 2)
+        self.assertLength(0, 1)
+        self.assertLength(1, 2)
+        self.assertLength(127, 2)
+        self.assertLength(128, 3)
+        self.assertLength(-128, 2)
+        self.assertLength(-129, 3)
+
+
+class RAM(Base):
+
+    def setUp(self):
+        self.db = stash.RAM()
+
+
+class FsDB(Base):
+
+    def setUp(self):
+        c = tempfile.TemporaryDirectory()
+        self.addCleanup(c.__exit__, None, None, None)
+        self.db = stash.FsDB(c.__enter__())
+
+
+class FileDB(Base):
+
+    def setUp(self):
+        c = tempfile.NamedTemporaryFile()
+        self.addCleanup(c.__exit__, None, None, None)
+        self.dbpath = c.__enter__().name
+        self.db = stash.FileDB(self.dbpath)
+
+    def test_reload(self):
+        obj1 = 1, 2, 3
+        obj2 = obj1, "abc"
+        h1 = self.db.hash(obj1)
+        h2 = self.db.hash(obj2)
+        del self.db
+        db = stash.FileDB(self.dbpath)
+        self.assertEqual(db.hash(obj1), h1)
+        self.assertEqual(db.hash(obj2), h2)
+
+
+del Base
